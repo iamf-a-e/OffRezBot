31691b7ec9d51f9b5292ad36853c1007aa91ecb8
Fae <109899113+iamf-a-e@users.noreply.github.com>
2025-06-11 16:50:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -444,5 +444,6 @@ action_mapping = {
 }
 
 
-if _name_ == "_main_":
-    app.run(debug=True, host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
+
